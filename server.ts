85d3de1010410ecf5f6bbe1bf175fa12d0679e73
allyksander <aleksandr.panchenko.1989@gmail.com>
2024-10-08 13:00:09 +0300

    feat: implement GET method

--- server.ts
+++ server.ts
@@ -3,7 +3,28 @@ import express from "express";
 import { Db } from "mongodb";
 import { connetToDb, getDb } from "./src/db";
 
+export interface Duration {
+  hours: number;
+  minutes: number;
+}
+
+export interface Review {
+  name: string;
+  text: string;
+}
+
+export interface Movie {
+  title: string;
+  director: string;
+  year: number;
+  genres: string[];
+  rating: number;
+  duration: Duration;
+  reviews: Review[];
+}
+
 const PORT = process.env.PORT || "3000";
+const MOVIES = "movies";
 const app = express();
 let db: Db;
 
@@ -15,5 +36,15 @@ connetToDb((error: Error) => {
     db = getDb();
   } else {
     console.log(`DB connection error: ${error}`);
+  }
+});
+
+app.get(`/${MOVIES}`, async (req, res) => {
+  try {
+    const movies = await db.collection<Movie>(MOVIES).find().toArray();
+
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).json({ error });
   }
 });
